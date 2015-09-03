cde2e5fe38b875231bbfb8be5f6918e432fc75cb
jandritsch <jandritsch@gettyimages.com>
2015-09-02 22:34:50 -0500

    Add tests for HistoryManager

---
+++ src/test/scala/HistoryManagerSpec.scala
@@ -0,0 +1,46 @@
+package com.jandritsch.productsearch
+
+import org.scalatest._
+import java.io.{File, BufferedWriter, FileWriter}
+
+class HistoryManagerSpec extends FunSpec with BeforeAndAfter {
+
+  val historyFilePath = "src/test/resources/history.json"
+
+  before {
+    // Set up sample history file
+    val file = new File(historyFilePath)
+    val bw = new BufferedWriter(new FileWriter(file))
+    bw.write(
+      """
+      { "history": [] }
+      """
+      )
+    bw.close()
+  }
+
+  describe("#addHistory") {
+
+    it("accepts a search term and whether or not it was found and writes it to history") {
+      val historyManager = new HistoryManager(historyFilePath)
+
+      historyManager.addHistory("product1", false)
+      historyManager.addHistory("product2", true)
+      var savedHistory = FileUtils.readFileAsJson(historyFilePath)
+      val entries = (savedHistory \ "history").children
+      assert(entries.length === 2)
+
+      val entry1 = entries(0)
+      val entry2 = entries(1)
+
+      assert((entry1 \ "name").values === "product1")
+      assert((entry1 \ "found").values === false)
+      assert((entry1 \ "searchedAt").values !== None)
+
+      assert((entry2 \ "name").values === "product2")
+      assert((entry2 \ "found").values === true)
+      assert((entry2 \ "searchedAt").values !== None)
+    }
+
+  }
+}
